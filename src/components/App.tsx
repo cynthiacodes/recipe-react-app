43fc41d08189e14a0e1233022294e07f6c9837d8
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-14 15:51:49 +0100

    centered the layout and added new recipe button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,20 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Button, Grid, Heading, Stack } from "@chakra-ui/react";
 import "./App.css";
 import { RecipeCard } from "./RecipeCard";
 
 function App() {
     return (
-        <div className="App">
-            <Heading as="h1" size="4xl" noOfLines={1}>
-                Cynthia's Recipe App
-            </Heading>
+        <Grid placeItems="center" minHeight="100vh">
+            <Box>
+                <Heading as="h1" size="4xl" noOfLines={1}>
+                    Recipe App
+                </Heading>
+                <Stack align={"center"}>
+                    <Button>Add New Recipe</Button>
+                </Stack>
+            </Box>
             <RecipeCard />
-        </div>
+        </Grid>
     );
 }
 
