615382c3668b8f7346a44ccacb68e2399dfc141f
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-14 15:31:10 +0100

    added heading for web page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
+import { Heading } from "@chakra-ui/react";
 import "./App.css";
 import { RecipeCard } from "./RecipeCard";
 
 function App() {
     return (
         <div className="App">
+            <Heading as="h1" size="4xl" noOfLines={1}>
+                Cynthia's Recipe App
+            </Heading>
             <RecipeCard />
         </div>
     );
