7206eefdead2f6d53e6e3c9d442c98862a586630
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-14 15:31:35 +0100

    rendering each recipe as a card

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,12 +1,34 @@
 import axios from "axios";
 import { apiBaseUrl } from "./apiBaseUrl";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    CardBody,
+    CardFooter,
+    Divider,
+    Heading,
+    SimpleGrid,
+    Stack,
+    Text,
+    useBreakpointValue,
+} from "@chakra-ui/react";
+
+interface Recipe {
+    id: number;
+    title: string;
+    meal_type: string;
+    cuisine: string;
+    video_url: string;
+}
 export function RecipeCard(): JSX.Element {
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
     async function getAllRecipes() {
         try {
             const response = await axios.get(apiBaseUrl + "/recipes");
             const allRecipes = response.data;
-            console.log(allRecipes);
+            setRecipes(allRecipes);
         } catch (error) {
             console.error(error);
         }
@@ -14,9 +36,50 @@ export function RecipeCard(): JSX.Element {
     useEffect(() => {
         getAllRecipes();
     }, []);
+
+    const handleVideoUrl = (videoUrl: string) => {
+        window.open(videoUrl);
+    };
+    const maxCardWidth = useBreakpointValue({ base: "sm", md: "md", lg: "xl" });
+    const recipeCards = recipes.map((recipe) => (
+        <Card maxW={maxCardWidth} key={recipe.id}>
+            <CardBody>
+                <Stack mt="6" spacing="3">
+                    <Heading size="md">{recipe.title}</Heading>
+                    <Text>{recipe.meal_type}</Text>
+                    <Text color="teal.600" fontSize="xl">
+                        {recipe.cuisine}
+                    </Text>
+                </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+                <ButtonGroup spacing="2">
+                    <Button
+                        variant="link"
+                        colorScheme="teal"
+                        onClick={() => handleVideoUrl(recipe.video_url)}
+                    >
+                        Tutorial
+                    </Button>
+                    <Button variant="ghost" colorScheme="teal" px={2} mx={1}>
+                        Edit
+                    </Button>
+                    <Button variant="ghost" colorScheme="teal" px={2} mx={1}>
+                        Delete
+                    </Button>
+                </ButtonGroup>
+            </CardFooter>
+        </Card>
+    ));
     return (
         <>
-            <h1>Recipe card</h1>
+            <SimpleGrid
+                spacing={4}
+                templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+            >
+                {recipeCards}
+            </SimpleGrid>
         </>
     );
 }
