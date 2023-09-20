e540c6a964c0f7029a2e01bfc1a1da0e02923a75
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-20 19:11:43 +0100

    lifted recipe state and getAllRecipesFn

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,9 +9,27 @@ import {
 import "./App.css";
 import { NewRecipe } from "./NewRecipe";
 import { RecipeCard } from "./RecipeCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { RecipeWithId } from "./Interface";
+import { apiBaseUrl } from "./apiBaseUrl";
 
 function App() {
+    const [recipes, setRecipes] = useState<RecipeWithId[]>([]);
     const { isOpen, onClose, onOpen } = useDisclosure();
+    async function getAllRecipes() {
+        try {
+            const response = await axios.get(apiBaseUrl + "/recipes");
+            const allRecipes = response.data;
+            setRecipes(allRecipes);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        getAllRecipes();
+    }, []);
+
     return (
         <Grid placeItems="center" minHeight="100vh">
             <Box>
@@ -21,9 +39,13 @@ function App() {
                 <Stack align={"center"}>
                     <Button onClick={onOpen}>Add New Recipe</Button>
                 </Stack>
-                <NewRecipe isOpen={isOpen} onClose={onClose} />
+                <NewRecipe
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    getAllRecipes={getAllRecipes}
+                />
             </Box>
-            <RecipeCard />
+            <RecipeCard getAllRecipes={getAllRecipes} recipes={recipes} />
         </Grid>
     );
 }
